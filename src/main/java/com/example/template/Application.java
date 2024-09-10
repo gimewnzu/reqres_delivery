1423110a33668451aa01fcb4d7e87044b41fdf71
gimewnzu <gimewn164347@gmail.com>
2024-09-10 16:21:06 +0900

    Update Application.java

--- src/main/java/com/example/template/Application.java
+++ src/main/java/com/example/template/Application.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.ApplicationContext;
-
+  
 @SpringBootApplication
 @EnableFeignClients
 public class Application {
